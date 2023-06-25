c24efefac0d2dd46537c14877315509a5cddd05c
Jefferson Balde <jeffersonbalde8@gmail.com>
2023-06-25 11:18:27 +0800

    code for java jdbc

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        CREATE A DATABASE FIRST
+        String url = "jdbc:mysql://localhost:3306/yourdatabasename";
+//        database username
+        String username = "root";
+//        database password
+        String password = "root";
+
+        try{
+//            download java mysql connector in chrome and put in your as a jar folder libraries
+           Class.forName("com.mysql.cj.jdbc.Driver");
+
+           Connection connection = DriverManager.getConnection(url, username, password);
+
+           Statement statement = connection.createStatement();
+
+           ResultSet resultSet = statement.executeQuery("SELECT * FROM yourdatabasetablename");
+
+           while(resultSet.next()){
+               System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3));
+           }
+
+           connection.close();
+
+        }catch (Exception e){
+            System.out.println(e);
+        }
     }
 }
